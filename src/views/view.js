371c280f72d8662f3c7a5397ff238917719ec465
Natalia <nataliamh.88@gmail.com>
2018-08-14 15:37:57 -0300

    avance segunda version

--- src/views/view.js
+++ src/views/view.js
@@ -1,4 +1,5 @@
 const containerList = document.getElementsByClassName('list')[0];
+const containerTitle = document.getElementsByClassName('textTitle')[0];
 
 function hideAndShowContent() {
     const addAList = document.getElementById('addListForInput')
@@ -21,7 +22,7 @@ function createInputForTitle() {
     buttonAddList.setAttribute('class', 'styleButtonList')
     buttonDelete.setAttribute('class', 'buttonDeleteStyle')
     iconDelete.setAttribute('class', 'fas fa-times')
-    buttonAddList.onclick = function() { createTitleList() };
+    buttonAddList.onclick = function() { hideAndShowContentTwo() };
  
     containerList.appendChild(inputList);
     containerList.appendChild(buttonAddList);
@@ -32,20 +33,27 @@ function createInputForTitle() {
 
     function hideAndShowContentTwo(){
         containerList.style.display = 'none';
-        const 
+        containerTitle.style.display = 'block';
+        createTitleList();
     };
 
     function createTitleList(){
     const bringInputTitle = document.getElementsByClassName('inputListForTitle')[0].value;
   
-    const titleNameDiv = document.createElement('div');
     const titleNamePar = document.createElement('p');
     const beATextTitle = document.createTextNode(bringInputTitle);
+    const addACard = document.createElement('a');
+    const iconAdd = document.createElement('i');
+    const textAdd = document.createTextNode(' Add a card');
 
-    titleNamePar.setAttribute('class', 'styleTitle');
-    titleNameDiv.setAttribute('class', 'textTitle')
-
+    iconAdd.setAttribute('class', 'iconAddCard fas fa-plus')
+    addACard.setAttribute('class', 'addCardLink')
+    titleNamePar.setAttribute('class', 'title'); 
+    
+    containerTitle.appendChild(titleNamePar);
     titleNamePar.appendChild(beATextTitle);
-    titleNameDiv.appendChild(titleNamePar);
-    // containerList.appendChild(titleNameDiv);
+    addACard.appendChild(iconAdd);
+    addACard.appendChild(textAdd);
+    containerTitle.appendChild(addACard);
+    
 }
