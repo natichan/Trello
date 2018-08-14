bc9f623f22920ca0549eea928e3788c2cfc0967a
Natalia <nataliamh.88@gmail.com>
2018-08-14 14:43:59 -0300

    creando version 0.02

--- src/views/view.js
+++ src/views/view.js
@@ -1,31 +1,51 @@
-function createNewInput() {
+const containerList = document.getElementsByClassName('list')[0];
+
+function hideAndShowContent() {
     const addAList = document.getElementById('addListForInput')
-    const containerList = document.getElementsByClassName('list')[0];
     addAList.style.display = 'none';
     containerList.style.display = 'block';
-    createTodoList();
+    createInputForTitle();
   };
   
-function createTodoList() {
-    const containerList = document.getElementsByClassName('list')[0];
+function createInputForTitle() {
     const inputList = document.createElement('input');
     const buttonAddList = document.createElement('button');
     const nameButtonAddList = document.createTextNode('Add List');
     const buttonDelete = document.createElement('button');
     const iconDelete = document.createElement('i');
 
-    inputList.setAttribute('class', 'inputList');
+    inputList.setAttribute('class', 'inputListForTitle');
     inputList.setAttribute('placeholder', ' Enter list title...');
 
     inputList.type = 'text';
     buttonAddList.setAttribute('class', 'styleButtonList')
     buttonDelete.setAttribute('class', 'buttonDeleteStyle')
     iconDelete.setAttribute('class', 'fas fa-times')
-    // buttonAddList.onclick = function() { runCommand() };
+    buttonAddList.onclick = function() { createTitleList() };
  
     containerList.appendChild(inputList);
     containerList.appendChild(buttonAddList);
     buttonAddList.appendChild(nameButtonAddList);
     containerList.appendChild(buttonDelete);
     buttonDelete.appendChild(iconDelete);
+};
+
+    function hideAndShowContentTwo(){
+        containerList.style.display = 'none';
+        const 
+    };
+
+    function createTitleList(){
+    const bringInputTitle = document.getElementsByClassName('inputListForTitle')[0].value;
+  
+    const titleNameDiv = document.createElement('div');
+    const titleNamePar = document.createElement('p');
+    const beATextTitle = document.createTextNode(bringInputTitle);
+
+    titleNamePar.setAttribute('class', 'styleTitle');
+    titleNameDiv.setAttribute('class', 'textTitle')
+
+    titleNamePar.appendChild(beATextTitle);
+    titleNameDiv.appendChild(titleNamePar);
+    // containerList.appendChild(titleNameDiv);
 }
