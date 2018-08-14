c352bba740c38efbbc3231348a56ecd07a800cf5
Natalia <nataliamh.88@gmail.com>
2018-08-14 12:26:00 -0300

    estilo de input listo con icono

--- src/views/view.js
+++ src/views/view.js
@@ -8,15 +8,24 @@ function createNewInput() {
   
 function createTodoList() {
     const containerList = document.getElementsByClassName('list')[0];
-    const inputList = document.createElement('input')
-    const buttonAddList = document.createElement('button')
-    const nameButtonAddList = document.createTextNode('Add List')
+    const inputList = document.createElement('input');
+    const buttonAddList = document.createElement('button');
+    const nameButtonAddList = document.createTextNode('Add List');
+    const buttonDelete = document.createElement('button');
+    const iconDelete = document.createElement('i');
 
     inputList.setAttribute('class', 'inputList');
+    inputList.setAttribute('placeholder', ' Enter list title...');
+
     inputList.type = 'text';
     buttonAddList.setAttribute('class', 'styleButtonList')
-
+    buttonDelete.setAttribute('class', 'buttonDeleteStyle')
+    iconDelete.setAttribute('class', 'fas fa-times')
+    // buttonAddList.onclick = function() { runCommand() };
+ 
     containerList.appendChild(inputList);
     containerList.appendChild(buttonAddList);
     buttonAddList.appendChild(nameButtonAddList);
+    containerList.appendChild(buttonDelete);
+    buttonDelete.appendChild(iconDelete);
 }
